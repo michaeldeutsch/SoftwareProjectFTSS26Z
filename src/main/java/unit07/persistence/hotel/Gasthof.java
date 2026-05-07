dfdf86f8e0b3fa39c56e8d656c8e73bd681eb24d
mide <37365432+michaeldeutsch@users.noreply.github.com>
2026-05-07 15:23:37 +0200

    add unit06 GUI: implement main frame with hotel table, editing window, and integrated table model using Lombok for hotel data

---
+++ src/main/java/unit07/persistence/hotel/Gasthof.java
@@ -0,0 +1,21 @@
+package unit07.persistence.hotel;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@AllArgsConstructor
+@RequiredArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Gasthof {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int gasthofID;
+    @NonNull
+    @Column(unique = true)
+    private String gasthofName;
+
+}
---
+++ src/main/java/unit07/persistence/hotel/GasthofMain.java
@@ -0,0 +1,121 @@
+package unit07.persistence.hotel;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import unit07.persistence.user.UserEntity;
+
+import java.util.List;
+
+public class GasthofMain {
+
+    private static final SessionFactory SESSION_FACTORY = HibernateUtil.getSessionFactory();
+
+
+    public static void main(String[] args) {
+
+        /*
+        Gasthof g = Gasthof.builder().gasthofName("Reinthaler").build();
+        System.out.println(g);
+        System.out.println(g1);
+
+        Gasthof temp = createGasthof(g);
+        System.out.println(temp);
+
+        deleteGasthof(1);
+
+        Gasthof g1 = new Gasthof("Stüberl");
+        createGasthof(g1);
+*/
+        findByName("Stüberl");
+    }
+
+    private static void findByName(String term ) {
+        Transaction tx = null;
+
+        try(Session session = SESSION_FACTORY.openSession()){
+            tx = session.beginTransaction();
+
+            String hql = "from Gasthof where gasthofName = :term";
+
+          Gasthof g=   session.createQuery(hql, Gasthof.class).setParameter("term", term).uniqueResult();
+            System.out.println("Was found " + g);
+            tx.commit();
+        }catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+
+    private static void deleteGasthof(int i) {
+
+        Transaction tx = null;
+
+        try(Session session = SESSION_FACTORY.openSession()){
+            tx = session.beginTransaction();
+            session.delete(session.get(Gasthof.class, i));
+            tx.commit();
+            System.out.println("Gasthof deleted");
+        }catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private static Gasthof createGasthof(Gasthof g) {
+
+        Transaction tx = null;
+
+        try(Session session = SESSION_FACTORY.openSession()) {
+            tx = session.beginTransaction();
+            session.persist(g);
+            tx.commit();
+            return g;
+        }catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            e.printStackTrace();
+        }
+        return null;
+
+
+    }
+
+
+    private static void saveAll(List<Gasthof> gasthofs) {
+
+        Transaction tx = null;
+        int batchSize = 50;
+
+        try(Session session = SESSION_FACTORY.openSession()) {
+            tx = session.beginTransaction();
+
+            for (int i = 0; i < gasthofs.size(); i++) {
+                session.persist(gasthofs.get(i));
+                if (i % batchSize == 0) {
+                    session.flush();
+                    session.clear();
+                }
+            }
+
+
+            tx.commit();
+
+        }catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            e.printStackTrace();
+        }
+
+
+
+    }
+}
